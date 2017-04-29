c0b2c727fec4514d30bf0e8206831e3d6a3f87aa
Philip Winchester <philipwinc@gmail.com>
2017-04-29 13:25:01 -0400

    added constructor to App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      test: ''
+    }
+  }
+
   render() {
     return (
       <div>
