c39b59f154e161326f9f78a06d5191943220463e
Philip Winchester <philipwinc@gmail.com>
2017-06-03 15:18:18 -0400

    initialing router. Added empty dirs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import App      from './components/App.jsx';
 
+//React Router imports
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
 // mount our App at #root-container
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root-container')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.querySelector('#root-container')
+// );
+ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Login}/>
+        <Route path="/user" component={Profile} />
+        <Route path="/host" component={Host} />
+      </Route>
+    </Router>
+  ), document.querySelector('#root-container'));
