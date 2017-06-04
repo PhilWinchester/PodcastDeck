3d58efbf71960af4e1acee95556b8639dc5b37e6
Philip Winchester <philipwinc@gmail.com>
2017-06-03 20:10:49 -0400

    added Search Bar componenet and holder for original App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
-import PodcastItem from './PodcastItem/PodcastItem';
 import './App.css';
 
 export default class App extends Component {
@@ -8,67 +7,31 @@ export default class App extends Component {
     super();
 
     this.state = {
-      podcastTitle: 'the daily',
-      podcasts: null
+      episodeQueue: [],
+      user: {
+        username: '',
+        id: ''
+      }
     }
   }
 
-  handleKeyPress(target){
-    if (target.charCode === 13) {
-      this.fireItunesFetch()
-    }
+  tempFunction() {
+    console.log('Temp Function');
   }
-
-  fireItunesFetch() {
-    let convertedTitle = this.state.podcastTitle.replace(/\s/g, '+');
-
-    fetch(`/podcast/search`, {
-      headers: {
-        'Content-Type':'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        podcastTitle: convertedTitle
-      }),
-    })
-    .then(r => r.json())
-    .then(resp => {
-      const podcastArr = resp.results.map((elem, counter) => (
-        <PodcastItem
-          key={counter}
-          collectionName={elem.collectionName}
-          artistName={elem.artistName}
-          artworkUrl={elem.artworkUrl100}
-          feedUrl={elem.feedUrl}
-        />
-      ))
-
-      this.setState({
-        podcasts: podcastArr
-      });
-
-    })
-    .catch(err => console.log(err));
-  }
-
-  handleTitleInputChange(e) {
-    this.setState({
-      podcastTitle: e.target.value
-    })
-  }
-
 
   render() {
     return (
       <div>
-        <input type='search' placeholder='Title'
-          onChange={(e) => this.handleTitleInputChange(e)}
-          onKeyPress={(t) => this.handleKeyPress(t)}
-        ></input>
-        <hr></hr>
-        <button onClick={() => this.fireItunesFetch()} >Query iTunes</button>
-        <hr></hr>
-        {this.state.podcasts}
+        <div className='sidebar'>
+          <h1>Podcast Deck</h1>
+          {/* <SearchBar/> */}
+        </div>
+
+        {this.props.children && React.cloneElement(this.props.children, {
+          appState: this.state,
+          tempFunction: this.tempFunction.bind(this)
+        })}
+
       </div>
     );
   }
---
+++ src/initialApp.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import PodcastItem from './PodcastItem/PodcastItem';
+import './App.css';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      podcastTitle: 'the daily',
+      podcasts: null
+    }
+  }
+
+  handleKeyPress(target){
+    if (target.charCode === 13) {
+      this.fireItunesFetch()
+    }
+  }
+
+  fireItunesFetch() {
+    let convertedTitle = this.state.podcastTitle.replace(/\s/g, '+');
+
+    fetch(`/podcast/search`, {
+      headers: {
+        'Content-Type':'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        podcastTitle: convertedTitle
+      }),
+    })
+    .then(r => r.json())
+    .then(resp => {
+      const podcastArr = resp.results.map((elem, counter) => (
+        <PodcastItem
+          key={counter}
+          collectionName={elem.collectionName}
+          artistName={elem.artistName}
+          artworkUrl={elem.artworkUrl100}
+          feedUrl={elem.feedUrl}
+        />
+      ))
+
+      this.setState({
+        podcasts: podcastArr
+      });
+
+    })
+    .catch(err => console.log(err));
+  }
+
+  handleTitleInputChange(e) {
+    this.setState({
+      podcastTitle: e.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type='search' placeholder='Title'
+          onChange={(e) => this.handleTitleInputChange(e)}
+          onKeyPress={(t) => this.handleKeyPress(t)}
+        ></input>
+        <hr></hr>
+        <button onClick={() => this.fireItunesFetch()} >Query iTunes</button>
+        <hr></hr>
+        {this.state.podcasts}
+      </div>
+    );
+  }
+}
